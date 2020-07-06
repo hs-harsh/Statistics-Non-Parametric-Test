1d3508706199eb0ca22b8804b6f8b65ef84796ef
hs-harsh <harshsingh.iitd@gmail.com>
2020-07-06 06:00:43 +0530

    Regression Changes

--- Linear-Regression.py
+++ Linear-Regression.py
@@ -16,11 +16,19 @@ Xi4 = sum([x**4 for x in X])
 XiYi = sum([X[i]*Y[i] for i in range(n)])
 Xi2Yi = sum([(X[i]**2)*Y[i] for i in range(n)])
 
+print("Xi Yi : "+str(Xi)+" "+str(Yi))
+print("Xi2 : "+str(Xi2)+" "+str([x**2 for x in X]))
+print("Xi3 : "+str(Xi3)+" "+str([x**3 for x in X]))
+print("Xi4 : "+str(Xi4)+" "+str([x**4 for x in X]))
+print("XiYi : "+str(XiYi)+" "+str([X[i]*Y[i] for i in range(n)]))
+print("Xi2Yi : "+str(Xi2Yi)+" "+str([(X[i]**2)*Y[i] for i in range(n)]))
+
 # A*Beta = B  || Beta --> coefficients
 A = np.array([[n, Xi, Xi2],
               [Xi, Xi2, Xi3],
               [Xi2, Xi3, Xi4]])
 B = np.array([Yi, XiYi, Xi2Yi]).T
+print("--------------------------------------------")
 print(A)
 print(B)
 Beta = np.dot(B, np.linalg.inv(A))
